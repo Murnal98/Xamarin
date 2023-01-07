50eb3c1581f85730d8748f9a398b1b62cbcc90d8
Jaishree_Malode16 <96593494+Murnal98@users.noreply.github.com>
2023-01-08 00:50:43 +0530

    Models

---
+++ AllowanceModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace EmployeeAllowanceApp.Model
+{
+ public class AllowanceModel
+    {
+        [PrimaryKey, AutoIncrement]
+        public int serialNo { get; set; }
+        public long Employeekey { get; set; }
+
+        public int AllowanceId { get; set; }
+        
+        public DateTime ClockDate { get; set; }
+
+        public string AllowanceName { get; set; }
+
+        public decimal AllowanceAmount { get; set; }
+        public bool isUpload { get; set; }
+
+
+    }
+}
+
---
+++ AttendanceModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeAllowanceApp.Model
+{
+   public partial class AttendanceModel
+    {
+        //public string ClockDate { get; set; }
+
+        public long Employeekey { get; set; }
+
+        public string EmployeeName { get; set; }
+
+        public bool IsSelected { get; internal set; }
+    }
+}
